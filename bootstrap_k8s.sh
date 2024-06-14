b8d630dbbcb1c924a8b0badb5b73d218d0951f9f
Chen Chen <40508171+LukasChenChen@users.noreply.github.com>
2024-06-14 16:51:02 +0100

    Update bootstrap_k8s.sh

--- bootstrap_k8s.sh
+++ bootstrap_k8s.sh
@@ -42,4 +42,50 @@ ufw allow 6443
 ufw allow 6443/tcp
 
 rm /etc/containerd/config.toml
-systemctl restart containerd
+systemctl restart contained
+
+-----------------------------------
+#update 14.06.2024
+apt install -y apt-transport-https curl
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+sudo apt-get update
+
+apt-get install -y kubelet kubeadm kubectl
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+#see more https://kubernetes.io/blog/2023/08/15/pkgs-k8s-io-introduction/#how-to-migrate
+
+swapoff -a
+
+lsmod | grep br_netfilter
+
+modprobe br_netfilter
+
+sysctl net.bridge.bridge-nf-call-iptables=1
+
+sysctl net.ipv4.ip_forward= 1
+	
+mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{ "exec-opts": ["native.cgroupdriver=systemd"],
+"log-driver": "json-file",
+"log-opts":
+{ "max-size": "100m" },
+"storage-driver": "overlay2"
+}
+EOF
+
+systemctl enable docker
+systemctl daemon-reload
+systemctl restart docker
+
+ufw allow 6443
+ufw allow 6443/tcp
+
+rm /etc/containerd/config.toml
+systemctl restart contained
